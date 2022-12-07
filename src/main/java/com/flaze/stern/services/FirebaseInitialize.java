ad0f4b698830ac30b3c22eeeac6bbd7efa05a6a4
NotChamath <chamathwanigasooriya@gmail.com>
2022-12-07 19:18:48 +0530

    fix FirebaseInitialize resource not found issue

--- src/main/java/com/flaze/stern/services/FirebaseInitialize.java
+++ src/main/java/com/flaze/stern/services/FirebaseInitialize.java
@@ -3,13 +3,12 @@ package com.flaze.stern.services;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.InputStream;
+import java.util.Objects;
 
 @Service
 public class FirebaseInitialize {
@@ -22,11 +21,8 @@ public class FirebaseInitialize {
     @PostConstruct
     public void initialize() {
         try {
-            Resource resource = resourceLoader.getResource("classpath:static/sync-vote-firebase-adminsdk-1ej20-f2c1095069.json");
-            File file = new File(resource.getFile().toURI());
-            InputStream serviceAccount = resource.getInputStream();
-            FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).setDatabaseUrl("https://sync-vote-default-rtdb.asia-southeast1.firebasedatabase.app").build();
-
+            InputStream resource = FirebaseInitialize.class.getClassLoader().getResourceAsStream("static/sync-vote-firebase-adminsdk-1ej20-f2c1095069.json");
+            FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(Objects.requireNonNull(resource))).setDatabaseUrl("https://sync-vote-default-rtdb.asia-southeast1.firebasedatabase.app").build();
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
             }
